2324f5a56df53e8d1777e61ebb8e00149c4e28ce
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-02 15:32:27 -0600

    Update Nav style

--- practiceapp/client/src/components/Navbar.js
+++ practiceapp/client/src/components/Navbar.js
@@ -1,26 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Style from "./Style.css";
 
-function Navbar () {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <span className="navbar-brand" href="#">
+        Google Books
+      </span>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-                <Link to="/" className="nav-link">Saved<span className="sr-only">(current)</span></Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/search" className="nav-link">Search</Link>
-            </li>
-            </ul>
-        </div>
-        </nav>
-    )
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link to="/" className="nav-link">
+              Saved<span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/search" className="nav-link">
+              Search
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
